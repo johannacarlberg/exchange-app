d2e39719e6e816bb93061c9cea9add6166286693
Jo <jscarlberg@gmail.com>
2018-10-11 21:00:19 +0100

    added linting and first screen

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
-console.log('Initial setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-var path = require("path");
-var DIST_DIR = path.resolve(__dirname, "dist");
-var SRC_DIR = path.resolve(__dirname, "src");
+const path = require('path');
 
-var config = {
-    entry: SRC_DIR + "/app/index.js",
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+const config = {
+    entry: SRC_DIR + '/app/index.js',
     output: {
-        path: DIST_DIR + "/app",
-        filename: "bundle.js",
-        publicPath: "/app/"
+        path: DIST_DIR + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
     },
     module: {
         rules: [
             {
                 test: /\.js?/,
                 include: SRC_DIR,
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 query: {
-                    presets: ["react", "es2015", "stage-2"]
+                    presets: ['react', 'es2015', 'stage-2']
                 }
             }
         ]
