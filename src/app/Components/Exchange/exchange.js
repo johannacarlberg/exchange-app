57c286cbe6e8f90c506ea922e03bc47dbaa1ddbc
Jo <jscarlberg@gmail.com>
2018-10-16 11:02:57 +0100

    to and from currency components and requesting data from server

--- src/app/Components/Exchange/exchange.js
+++ src/app/Components/Exchange/exchange.js
@@ -1,7 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
-export default class Exchange extends Component {
-    render(){
-        return <p>💅</p> 
+import FromCurrency from './FromCurrency/FromCurrency';
+import ToCurrency from './ToCurrency/ToCurrency';
+
+const mapStateToProps = state => {
+  return {state}
+};
+
+export class connectedExchange extends Component {
+    constructor() {
+        super();
+        this.state = {
+          rates: [],
+          fromCurrency: 'GBP',
+          toCurrency: 'EUR'
+        };
+      }
+
+      componentDidMount() {
+        this.requestRates(this.props.state.fromCurrency)
+      }
+
+      async callApi(baseRate){
+        const response = await fetch('/api/rates/' + baseRate)
+        const body = await response.json();
+    
+        if (response.status !== 200) throw Error(body.message);
+    
+        return body;
+      };
+
+    requestRates = function requestRates(baseRate) {
+      this.callApi(this.props.state.fromCurrency)
+      .then(res => {
+        console.log('rates', res.rates)
+        this.setState({ rates: res.rates})
+      })
+      .catch(err => console.log(err));
+    }
+     
+    render () {
+        return (
+            <div>
+                <FromCurrency fromCurrency={this.props.state.fromCurrency} value={0} />
+                <ToCurrency toCurrency={this.props.state.toCurrency} value={2} />
+            </div>
+        )
     }
 }
+
+const Exchange = connect(mapStateToProps, null)(connectedExchange);
+
+export default Exchange;
