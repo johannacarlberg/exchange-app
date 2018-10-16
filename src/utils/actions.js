cd26bf9e82e8eebb4e6fc665fe293004fc502ab1
Jo <jscarlberg@gmail.com>
2018-10-16 18:15:51 +0100

    updated actions

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -1,13 +1,33 @@
-import {SET_TO_CURRENCY, SET_FROM_CURRENCY, SWAP_CURRENCIES } from './constants';
+import {SET_TO_CURRENCY, SET_FROM_CURRENCY, SET_TO_BALANCE, SET_FROM_VALUE, SET_TO_VALUE, SET_FROM_BALANCE, SWAP_CURRENCIES } from './constants';
 
 export const setToCurrency = currency => ({
   type: SET_TO_CURRENCY,
   payload: currency
 });
 
-export const setFromCurrency = fromCurrency => ({
+export const setFromCurrency = currency => ({
   type: SET_FROM_CURRENCY,
-  payload: fromCurrency
+  payload: currency
+});
+
+export const setToBalance = balance => ({
+  type: SET_TO_BALANCE,
+  payload: balance
+});
+
+export const setFromBalance = balance => ({
+  type: SET_FROM_BALANCE,
+  payload: balance
+});
+
+export const setFromValue = value => ({
+  type: SET_FROM_VALUE,
+  payload: value
+});
+
+export const setToValue = value => ({
+  type: SET_TO_VALUE,
+  payload: value
 });
 
 export const swapCurrencies = currencies => ({
