5ea1c2b95db32eb3f856a9ff6fc49d9976d12bfc
Jo <jscarlberg@gmail.com>
2018-10-16 15:04:34 +0100

    separated out styling from tocurrency

---
+++ src/app/Components/ToCurrency/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const BalanceText = styled.p`
+  font-size: 12px;
+  color: ${props => props.overDraft ? '#f44250' : '#535456'};
+`;
+
+export const Input = styled.input`
+  padding: 0.5em;
+  font-size: 25px;
+  color: "palevioletred";
+  background: none;
+  border: none;
+
+  &:focus{
+    border: none;
+  }
+`;
+
+export const Indicator = styled.i`
+  display: inline-block;
+  margin-left: 20px;
+
+  &:after{
+    font-size: 22px;
+    content: '+';
+  }
+`;
