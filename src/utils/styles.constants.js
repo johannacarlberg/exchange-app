9989167d741f26df536de462716c7d59a783b7c3
Johanna <jscarlberg@gmail.com>
2018-10-17 12:14:31 +0100

    added constants for styles

---
+++ src/utils/styles.constants.js
@@ -0,0 +1,8 @@
+export const DARK_GRAY = '#535456';
+export const WHITE = '#fff';
+export const PALE_PINK = '#e29ec7';
+export const PINK = '#eb008d';
+export const BLUE = '#4c6eb2';
+export const LIGHT_GRAY = '#f4f5f7';
+
+
