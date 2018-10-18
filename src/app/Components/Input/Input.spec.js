2f746d326305fc2316acf3a770e8a5459daed3ba
Johanna <jscarlberg@gmail.com>
2018-10-18 21:42:38 +0100

    updated snapshot

--- src/app/Components/Input/Input.spec.js
+++ src/app/Components/Input/Input.spec.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
 import Input from './Input';
 
 it('Snapshot - renders correctly', () => {
   const onChange = jest.fn();
   const tree = renderer
-    .create(<Input indicator="+" value="23" onChange={onChange} />)
+    .create(<Input indicator="-" value="23" onChange={onChange} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
