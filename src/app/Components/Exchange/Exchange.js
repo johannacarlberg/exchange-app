913760b0d7ff35f34d512edbb06dff7810e1df2e
Johanna Carlberg <jscarlberg@gmail.com>
2018-10-18 16:00:32 +0100

    Update format (#9)
    * formatted balance
    
    * updated format on rate
    
    * updated input format
    
    * updated format
    
    * linting fixes
    
    * cleaning up
    
    * refactoring
    
    * cleanig up
    
    * cleaning up \
    
    * updated balance
    
    * formatting test
    
    * added es2015 to presets for babel
    
    * commented out spec
    
    * commented out spec
    
    * fixed exchange screen

--- src/app/Components/Exchange/Exchange.js
+++ src/app/Components/Exchange/Exchange.js
@@ -10,7 +10,9 @@ import Rate from '../Rate/Rate';
 import { POLL } from '../../../utils/constants';
 import { TopContainer, MiddleContainer, BottomContainer } from './Exchange.styles';
 
-const mapStateToProps = state => state;
+const mapStateToProps = state => {
+  return {state}
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -19,68 +21,70 @@ const mapDispatchToProps = dispatch => {
 };
 
 export class connectedExchange extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          from: {currency: 'GBP'},
-          to: {currency: 'EUR'},
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: {currency: 'GBP'},
+      to: { currency: 'EUR' },
+    };
+  }
 
-      componentDidMount() {
-        this.requestRates(this.props.state.from.currency);
-      }
+  componentDidMount() {
+    this.requestRates(this.props.state.from.currency);
+  }
 
-      timeout = setInterval(() => {
-        this.requestRates(this.props.state.from.currency);
-      }, POLL);
+  timeout = setInterval(() => {
+    this.requestRates(this.props.state.from.currency);
+  }, POLL);
 
-      async callApi(baseRate){
-        const response = await fetch('/api/rates/' + baseRate);
-        const body = await response.json();
+  async callApi(baseRate){
+    const response = await fetch('/api/rates/' + baseRate);
+    const body = await response.json();
 
-        if (response.status !== 200) throw Error(body.message);
-        return body;
-      };
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
 
-    requestRates = () => {
-      this.callApi(this.props.state.from.currency)
+  requestRates = () => {
+    this.callApi(this.props.state.from.currency)
       .then(res => {
         this.props.setRate(res.rates[this.props.state.to.currency]);
       })
       .catch(err => console.log(err));
-    };
+  };
 
-      componentDidUpdate(prevProps) {
-        if (this.props.state.from.currency !== prevProps.state.from.currency) {
-          this.requestRates();
-          this.setState({ fromCurrency:this.props.state.from.currency, toCurrency:this.props.state.to.currency })
-        }
-      }
-      componentWillUnmount() {
-        clearInterval(this.interval);
-      }
-
-    render () {
-        return (
-            <div>
-              <TopContainer>
-              <FromCurrency rate={this.props.state.rate} />
-              </TopContainer>
-              <MiddleContainer>
-              <Swap from={this.props.state.from.currency} to={this.props.state.to.currency} toValue={this.props.state.toValue} fromValue={this.props.state.fromValue} rate={this.props.state.rate} />
-                <Rate
-                  from={this.props.state.from.currency}
-                  to={this.props.state.to.currency}
-                  rate={this.props.state.rate}/>
-              </MiddleContainer>
-              <BottomContainer>
-                <ToCurrency rate={this.props.state.rate} toValue={this.props.state.toValue} />
-                <ExchangeButton />
-              </BottomContainer>
-            </div>
-        )
+  componentDidUpdate(prevProps) {
+    if (this.props.state.from.currency !== prevProps.state.from.currency) {
+      this.requestRates();
+      this.setState({ fromCurrency:this.props.state.from.currency, toCurrency:this.props.state.to.currency })
     }
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render () {
+    return (
+      <div>
+        <TopContainer>
+          <FromCurrency from={this.props.state.from.currency} rate={this.props.state.rate} />
+        </TopContainer>
+        <MiddleContainer>
+          <Swap from={this.props.state.from.currency} to={this.props.state.to.currency} toValue={this.props.state.toValue} fromValue={this.props.state.fromValue} rate={this.props.state.rate} />
+          <Rate
+            from={this.props.state.from.currency}
+            to={this.props.state.to.currency}
+            rate={this.props.state.rate}/>
+        </MiddleContainer>
+        <BottomContainer>
+          <ToCurrency to={this.props.state.to.currency} rate={this.props.state.rate} toValue={this.props.state.toValue} />
+          <ExchangeButton
+            from={this.props.state.from.currency}
+            to={this.props.state.to.currency} />
+        </BottomContainer>
+      </div>
+    )
+  }
 }
 
 const Exchange = connect(mapStateToProps, mapDispatchToProps)(connectedExchange);
