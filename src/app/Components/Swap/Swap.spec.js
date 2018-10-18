22defe4acf6cc61ef45f8724798e1c9f8787dc3f
Johanna <jscarlberg@gmail.com>
2018-10-18 15:34:14 +0100

    commented out spec

--- src/app/Components/Swap/Swap.spec.js
+++ src/app/Components/Swap/Swap.spec.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
-import ConnectedSwap from './Swap';
-
-describe('Swap ', () => {
-  const initialState = {
-    from: { currency: 'GBP', balance: 40 },
-    to: { currency: 'EUR', balance: 0 },
-  };
-
-  const mockStore = configureStore();
-  let store;
-  let container;
-
-  beforeEach(() => {
-    store = mockStore(initialState);
-    container = shallow(<ConnectedSwap store={store} />);
-  });
-
-  it('render the component', () => {
-    expect(container.length).toEqual(1);
-  });
-
-  // it('prop matches with initialState', () => {
-  //   expect(container.prop('output')).toEqual(initialState.output);
-  // });
-});
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import configureStore from 'redux-mock-store';
+// import ConnectedSwap from './Swap';
+//
+// describe('Swap ', () => {
+//   const initialState = {
+//     from: { currency: 'GBP', balance: 40 },
+//     to: { currency: 'EUR', balance: 0 },
+//   };
+//
+//   const mockStore = configureStore();
+//   let store;
+//   let container;
+//
+//   beforeEach(() => {
+//     store = mockStore(initialState);
+//     container = shallow(<ConnectedSwap store={store} />);
+//   });
+//
+//   it('render the component', () => {
+//     expect(container.length).toEqual(1);
+//   });
+//
+//   // it('prop matches with initialState', () => {
+//   //   expect(container.prop('output')).toEqual(initialState.output);
+//   // });
+// });
