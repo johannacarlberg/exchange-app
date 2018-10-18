25204c9b2aad0f0400374996f1b1ea6f8a945fb0
Johanna <jscarlberg@gmail.com>
2018-10-18 14:10:10 +0100

    formatted balance

--- src/app/Components/Balance/Balance.js
+++ src/app/Components/Balance/Balance.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BalanceText } from './Balance.styles';
 
-const Balance = ({symbol, balance, insufficientCurrency}) => <BalanceText insufficientCurrency={insufficientCurrency}> Balance: {symbol}{Number(balance).toFixed(2)}</BalanceText>;
+const Balance = ({ symbol, balance, insufficientCurrency }) => (
+  <BalanceText insufficientCurrency={insufficientCurrency}>
+    {' '}
+    Balance:
+    {symbol}
+    {Number(balance).toFixed(2)}
+  </BalanceText>
+);
 
+
+Balance.propTypes = {
+  symbol: PropTypes.string.isRequired,
+  balance: PropTypes.number,
+  insufficientCurrency: PropTypes.bool,
+};
+
+Balance.defaultProps = {
+  balance: 0,
+  insufficientCurrency: false,
+};
 export default Balance;
